cb6a6844c83d8b4eb8f77f2af44e3f365d3fa520
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-01-25 22:21:43 -0800

    Rough calendar grid setup

--- ui/src/components/RoughCalendar.svelte
+++ ui/src/components/RoughCalendar.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import { onMount, tick } from "svelte";
 	import rough from 'roughjs';
 	import { annotate } from 'rough-notation';
-	let height = 300;
-    let canvasElement: HTMLCanvasElement
-    onMount(() => {
-        let rc = rough.canvas(canvasElement);
+	export let activeColor: string = 'limegreen';
+    let canvas: HTMLCanvasElement
+	let height: number = 100, width: number = 1100;
+    onMount(async () => {
+        let rc = rough.canvas(canvas);
+		canvas.width = width;
+		canvas.height = height;
+		await tick();
 
-		
-		
 
-		rc.rectangle(10, 10, 200, 200, {
-			fill: 'limegreen',
-			hachureAngle: 45,
-			hachureGap: 5,
-		});
-
-		rc.path('M230 80 A 45 45, 0, 1, 0, 275 125 L 275 80 Z', { fill: 'purple', hachureAngle: 60, hachureGap: 5 });
-		annotate(canvasElement, {
-			type: 'circle'
-		}).show();
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			// dark mode
+		}
+				
+		for (let x = 0; x < 73; x += 1) {
+			const col = x * 14 + 2;
+			for (let y = 0; y < 7; y += 1) {
+				const row = y * 14 + 2;
+				rc.rectangle(col, row, 10, 10, {
+					stroke: activeColor,
+					fill: activeColor,
+					hachureAngle: 33,
+					hachureGap: 3,
+				});
+			}
+		}
     });
 </script>
+<style>
+	.calendar {
+		margin: 1rem;
+	}
+</style>
 
-
-<canvas {height} bind:this={canvasElement}/>
-
+<div class="calendar">
+<canvas bind:this={canvas} />
+</div>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script ts>
-	import Calendar from "../components/Calendar.svelte";
 	import RoughCalendar from "../components/RoughCalendar.svelte";
 </script>
 
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
+
+
 <RoughCalendar />
-<Calendar />
+<RoughCalendar activeColor="gold" />
+<RoughCalendar activeColor="purple" />
