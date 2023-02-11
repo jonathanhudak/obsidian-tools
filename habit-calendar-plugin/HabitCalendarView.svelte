709abf68d0184af9149684b46336cc86c616da82
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-02-10 22:51:24 -0600

    Remove title. Note author can add it if wanted

--- habit-calendar-plugin/HabitCalendarView.svelte
+++ habit-calendar-plugin/HabitCalendarView.svelte
@@ -2,16 +2,6 @@
 	import HabitCalendar from "ui/src/components/HabitCalendar.svelte";
 	import type { DataViewHabit } from "ui/src/types";
 	export let habitsData: DataViewHabit[] = [];
-	export let habitName: string = "";
 </script>
 
-<div>
-	{habitName}
-	<HabitCalendar {habitsData} />
-</div>
-
-<style>
-	div {
-		position: relative;
-	}
-</style>
+<HabitCalendar {habitsData} />
