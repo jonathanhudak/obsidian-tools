9ccca2152ee4b0d00708eacc63086891b811830a
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-02-10 22:58:55 -0600

    Improve plugin deploy script

--- habit-calendar-plugin/deploy_plugin.sh
+++ habit-calendar-plugin/deploy_plugin.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # ENV
+SRC="$(pwd)"
 # https://gist.github.com/judy2k/7656bfe3b322d669ef75364a46327836?permalink_comment_id=3710638#gistcomment-3710638
-DEST="/Users/$USER/Library/Mobile Documents/iCloud~md~obsidian/Documents/Zettelkasten/.obsidian/plugins/obsidian-habit-calendar/"
-SRC="/Users/$USER/Code/obsidian-habit-calendar/habit-calendar-plugin/plugin/"
+
+# Target directory to your Obsidian vault
+DEST="/Users/$USER/Library/Mobile Documents/iCloud~md~obsidian/Documents/Zettelkasten/.obsidian/plugins"
 # BEGIN
 
-rm -rf plugin
-mkdir -p plugin
-cp main.js ./plugin/main.js
-cp manifest.json ./plugin/manifest.json
-cp versions.json ./plugin/versions.json
-cp styles.css ./plugin/styles.css
+echo $SRC
+
+rm -rf $SRC/plugin
+mkdir -p $SRC/plugin
+cp main.js $SRC/plugin/main.js
+cp manifest.json $SRC/plugin/manifest.json
+cp versions.json $SRC/plugin/versions.json
+cp styles.css $SRC/plugin/styles.css
 
 # COPY
 echo Rsync to $DEST
-# cp -R ./plugin "$p/"
-
-
 rsync -av --verbose --whole-file --times --exclude=".DS_Store" "$SRC" "$DEST"
